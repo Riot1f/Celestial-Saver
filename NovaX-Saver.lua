6776e951ccdc09c35e5aa03fc56196533524f279
Riot1f <gabeoneal04@gmail.com>
2025-08-06 05:49:03 -0400

    Update NovaX-Saver.lua

--- NovaX-Saver.lua
+++ NovaX-Saver.lua
@@ -14,12 +14,8 @@ local Tab = Window:CreateTab("Main")
 Tab:CreateButton({
     Name = "Copy",
     Callback = function()
-        local Params = {
-            RepoURL = "https://raw.githubusercontent.com/luau/SynSaveInstance/main/",
-            SSI = "saveinstance",
-        }
-        local synsaveinstance = loadstring(game:HttpGet(Params.RepoURL .. Params.SSI .. ".luau", true), Params.SSI)()
+        local SSI = loadstring(game:HttpGet("https://raw.githubusercontent.com/luau/SynSaveInstance/main/saveinstance.lua"))()
         local Options = {} -- See https://luau.github.io/UniversalSynSaveInstance/api/SynSaveInstance for options
-        synsaveinstance(Options)
+        SSI(Options)
     end
 })
